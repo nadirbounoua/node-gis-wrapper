f5419d00ab4ff1c35b19c33a98a38d3303ce27e7
Nadir Bounoua <anbounoua@gmail.com>
2019-07-25 18:38:56 +0200

    Updated example

--- example.js
+++ example.js
@@ -1,2 +1,3 @@
 var expa = require('./index.js')('email@example.com', 'PassWord123');
 expa.getToken().then(console.log).catch(console.log);
+expa.get('current_person.json').then(console.log).catch(console.log);
